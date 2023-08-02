754030e5214e98cb68779eee4b68268f48a4594a
pangmoo-KTW <KimTaewon>
2023-08-02 13:49:46 +0900

    initial commit

---
+++ ch2_1.c
@@ -0,0 +1,24 @@
+/* Add a retrieve function to the counting example
+ * from Listing 2-6 to retrieve the current value
+ * of counter.*/
+
+#include <stdio.h>
+
+int* retrieve(int *a) {
+    return a;
+}
+
+
+int* increment(void) {
+    static unsigned int counter = 0;
+    counter++;
+    retrieve(&counter);
+}
+
+int main(void) {
+    for (int i = 0; i < 5; i++) {
+        int* a = increment();
+        printf("%d\n", *a);
+    }
+    return 0;
+}
